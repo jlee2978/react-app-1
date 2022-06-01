c7a0b1f3661e7cb4ddc2da9072c5de371ffc8101
jlee2978 <35323949+jlee2978@users.noreply.github.com>
2022-06-01 01:27:09 -0400

    small changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from 'react';  // import object from module
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// old method (React 17)
+// const element = <h1>Hello World</h1>;       // component to render
+// ReactDom.render(element, document.getElementById('root'))      // redner(element you want to render, where to redner the element in the real DOM)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
